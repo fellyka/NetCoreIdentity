f6aaa1465677a8349171c3237d980605f5053512
fellyka <fellyka@gmail.com>
2024-05-06 12:10:48 +0200

    Using Identity API

--- IdentityApp/Pages/Identity/Admin/Dashboard.cshtml.cs
+++ IdentityApp/Pages/Identity/Admin/Dashboard.cshtml.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,13 +6,43 @@ namespace IdentityApp.Pages.Identity.Admin
 {
     public class DashboardModel : PageModel
     {
+        public DashboardModel(UserManager<IdentityUser> userMgr)
+          => UserManager = userMgr;
+        /*IdentityUser is a default class supplied by Microsoft but we can create our own by inheriting from this one*/
+        public UserManager<IdentityUser> UserManager { get; set; }
         public int UsersCount { get; set; } = 0;
         public int UsersUnconfirmed { get; set; } = 0;
         public int UsersLockedout { get; set; } = 0;
         public int UsersTwoFactor { get; set; } = 0;
 
-        public void OnGet()
+        private readonly string[] emails = { "alice@example.com", "bob@example.com", "charlie@example.com"};
+
+        public async Task<IActionResult> OnPostAsync()
         {
+            foreach(string email in emails)
+            {
+                /*The type of the user class is specified using the generic type argument to the AddDefaultIdentity method in the Program.cs- That type is IdentityUser
+                  The user class defines a set of properties that describe the user account and provide the data values that Identity needs to implement its features.
+                  To create a new user object, I create a new instance of the IdentityUser class and set three properties UserName, Email, and EmailConfirmed
+                 */
+                IdentityUser userObject = new IdentityUser
+                {
+                    UserName = email, //The email is used for both, UserName and Email
+                    Email = email,
+                    EmailConfirmed = true
+                };
+              IdentityResult result =  await UserManager.CreateAsync(userObject);
+                result.Process(ModelState);
+            }
+            if(ModelState.IsValid)
+            {
+                return RedirectToPage();
+            }
+
+            return Page();
+          
         }
+
+
     }
 }
---
+++ IdentityApp/Pages/Identity/IdentityExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.Runtime.CompilerServices;
+
+namespace IdentityApp.Pages.Identity;
+
+public static class IdentityExtensions
+{
+    public static bool Process(this IdentityResult result, ModelStateDictionary modelState)
+    {
+        foreach(IdentityError err in result.Errors ?? Enumerable.Empty<IdentityError>())
+        {
+            /*Each IdentityError obect in the sequence returned by the IdentityResult.Errors property defines a Code property and a Description property.
+             The Code property is used to unambiguously identify the error and is intended to be consumed by the application. The Description property 
+            describes an error that can be presented to the user. I use the foreach keyword to add the value from each IdentityError.Description property
+            and add it to the set of validation errors that ASP.NET Core will handle.
+             */
+            modelState.AddModelError(string.Empty, err.Description);
+        }
+        return result.Succeeded;    
+    }
+}
